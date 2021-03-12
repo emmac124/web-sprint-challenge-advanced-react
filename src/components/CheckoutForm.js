94e017df435052ac2ffbce113db7ede246a3a344
emmac124 <emma.cooperhawkins@gmail.com>
2021-03-12 09:48:14 -0800

    completed mvp and testing

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
+import useForm from './../hooks/useForm';
 
-const initialValue = {
+const initialValues = {
   firstName: "",
   lastName: "",
   address: "",
@@ -14,12 +15,10 @@ const initialValue = {
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
 const CheckoutForm = (props) => {
+  
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
 
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+  const [values, handleChanges] = useForm(initialValues);
 
   const handleSubmit = (e) => {
     e.preventDefault();
--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,37 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />);
+    const header = screen.queryByText(/checkout form/i);
+    expect(header).toBeInTheDocument();
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm />);
+
+    const firstName = screen.queryByLabelText(/first name:/i);
+    const lastName = screen.queryByLabelText(/last name:/i);
+    const address = screen.queryByLabelText(/address:/i);
+    const city = screen.queryByLabelText(/city:/i);
+    const state = screen.queryByLabelText(/state:/i);
+    const zip = screen.queryByLabelText(/zip:/i);
+    const button = screen.queryByRole('button', { name: /checkout/i });
+
+    userEvent.type(firstName, "Emma");
+    userEvent.type(lastName, "Cooper");
+    userEvent.type(address, "414 N Hodges Rd");
+    userEvent.type(city, "Spokane Valley");
+    userEvent.type(state, "Washington");
+    userEvent.type(zip, "99016");
+    userEvent.click(button);
+
+    const submitText = screen.queryByText(/emma/i, /cooper/i, /414 N Hodges Rd/i, /spokane valley/i, /washington/i, /99016/i);
+
+    expect(submitText).toBeInTheDocument();
+
+});
